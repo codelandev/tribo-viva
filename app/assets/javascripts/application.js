4064abca5664695d90a3d01149512b28e2e136ff
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-05-15 16:19:58 -0300

    Setup front-end files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
 //= require_tree ./lib
 //= require tribo_viva/tribo_viva.js
 //= require init.js
