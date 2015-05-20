e8d85e7b66f17a311b41893c063d5f2d8d1562bc
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-05-20 11:11:36 -0300

    Adds specs for offers_controller.rb

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,5 @@
 class OffersController < ApplicationController
-  helper_method :resource
-  def show;end
-
-  protected
-
-  def resource
-    @offer ||= Offer.find(params[:id])
+  def show
+    @offer = Offer.find(params[:id])
   end
 end
--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe OffersController, type: :controller do
+  let(:valid_offer) { Offer.make! }
 
+  describe "GET #home" do
+    it "returns http success when valid ID" do
+      get :show, id: valid_offer.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http error when invalid ID" do
+      get :show, id: '0'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end
