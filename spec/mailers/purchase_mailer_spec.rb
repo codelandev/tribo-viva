cb361d21cf755fe392cc1a1d8cbc6eaf8337982e
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-06-07 21:37:11 -0300

    Add PurchaseMailer basic tests

--- spec/mailers/purchase_mailer_spec.rb
+++ spec/mailers/purchase_mailer_spec.rb
@@ -1,17 +1,20 @@
 require "rails_helper"
 
 RSpec.describe PurchaseMailer, type: :mailer do
-  pending ".confirm" do
-    let(:mail) { PurchaseMailer.confirm }
+  describe ".confirm" do
+    let!(:purchase) { Purchase.make!(:confirmed) }
+    let!(:mail) { PurchaseMailer.confirm(purchase) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Confirm")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Confirmação de compra Tribo Viva")
+      expect(mail.to).to eq([purchase.user.email])
+      expect(mail.from).to eq(["confirmacao@triboviva.com.br"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Olá #{purchase.user.name}")
+      expect(mail.body.encoded).to match("Sua compra está pendente de pagamento, assim que o tiver, por favor, faça upload do comprovante no seguinte link")
+      expect(mail.body.encoded).to match("#{purchase_url(purchase.transaction_id)}")
     end
   end
 
