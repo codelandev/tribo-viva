b9495821e041353673d23a7265fca7c9303377c7
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-06-07 21:36:44 -0300

    Add `config.action_mailer.asset_host` to test and production environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'triboviva.com.br' }
+  config.action_mailer.asset_host = 'http://triboviva.com.br'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
