edf14c0869ff838585bf303f25a79d0f51bfe8dc
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-06-24 12:03:39 -0300

    Nullify offers relation in Producer

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,5 +1,5 @@
 class Producer < ActiveRecord::Base
-  has_many :offers
+  has_many :offers, dependent: :nullify
 
   validates :name, :address, :logo, :description, :contact_name, :phone, :email, presence: true
 
