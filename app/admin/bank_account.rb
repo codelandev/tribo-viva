a35d548f148c4dbaa1bd20c9dbb3deb105eaec37
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-06-24 12:02:30 -0300

    Permit owner_name and cnpj_cpf in admin/bank_account

--- app/admin/bank_account.rb
+++ app/admin/bank_account.rb
@@ -1,3 +1,4 @@
 ActiveAdmin.register BankAccount do
-  permit_params :cc, :bank, :agency, :bank_number, :operation_code
+  permit_params :cc, :bank, :agency, :bank_number, :operation_code, :owner_name,
+    :cnpj_cpf
 end
