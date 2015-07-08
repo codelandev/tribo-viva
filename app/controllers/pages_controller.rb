c8bc2fa0a0d8499406559493101757582f913453
Gabriel Carpenedo <cbgabe@gmail.com>
2015-07-07 23:16:37 -0300

    Improves valid_offer order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @valid_offers    = Offer.valid_offers
+    @valid_offers    = Offer.valid_offers.order(created_at: :desc)
     @finished_offers = Offer.finished_offers
   end
 
