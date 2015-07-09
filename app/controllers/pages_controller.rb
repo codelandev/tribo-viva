71d35c7d24105ee4f5232e641b1e155172f24e1d
Gabriel Carpenedo <cbgabe@gmail.com>
2015-07-09 20:09:28 -0300

    Improves homepage offer order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @valid_offers    = Offer.valid_offers.order(created_at: :desc)
-    @finished_offers = Offer.finished_offers.order(created_at: :desc).limit(6)
+    @valid_offers    = Offer.valid_offers.order(collect_starts_at: :asc)
+    @finished_offers = Offer.finished_offers.order(collect_starts_at: :desc).limit(6)
   end
 
   def about
