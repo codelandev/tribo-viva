c03ef461a19584f88c3118607d9301ee45904870
Gabriel Carpenedo <cbgabe@gmail.com>
2015-07-09 14:51:17 -0300

    Adds past offers to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @valid_offers    = Offer.valid_offers.order(created_at: :desc)
-    @finished_offers = Offer.finished_offers
+    @finished_offers = Offer.finished_offers.limit(6)
   end
 
   def about
