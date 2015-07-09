dc5c15103f368b00bddabed6ecfd1d2737ec5a34
Gabriel Carpenedo <cbgabe@gmail.com>
2015-07-09 15:26:48 -0300

    Improves past offers order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @valid_offers    = Offer.valid_offers.order(created_at: :desc)
-    @finished_offers = Offer.finished_offers.limit(6)
+    @finished_offers = Offer.finished_offers.order(created_at: :desc).limit(6)
   end
 
   def about
