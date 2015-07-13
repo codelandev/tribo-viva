f97e9e9fbf72f6dc9a5271fb1a61311878678615
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-07-13 18:12:48 -0300

    Fixes problems with admin/user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register User do
-  permit_params :cpf, :name, :email, :phone, :address
+  permit_params :cpf, :name, :email, :phone, :address, :password
   menu priority: 3
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :name
+      f.input :cpf
+      f.input :phone
+      f.input :address
+      f.input :password
+    end
+    f.actions
+  end
 end
