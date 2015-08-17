7762dfe29096827a593137c1f49caabd36b82689
Gabriel Carpenedo <gabriel.carpenedo@gmail.com>
2015-08-17 10:06:05 -0300

    Improves old offers limit in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @valid_offers    = Offer.valid_offers.order(collect_starts_at: :asc)
-    @finished_offers = Offer.finished_offers.order(collect_starts_at: :desc)
+    @finished_offers = Offer.finished_offers.order(collect_starts_at: :desc).limit(6)
   end
 
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815172801) do
+ActiveRecord::Schema.define(version: 20150817050840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
