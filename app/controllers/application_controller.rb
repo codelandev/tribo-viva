7e433a9dde7f5011473120f7b5cce2f4a9cf9ff4
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-11-10 09:53:51 -0200

    Blank lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   helper_method :cart_session
 
   protected
