c8c2f8b20906815b34469e12fa80be65eccdb23c
Patrick Müller <patrick.e.muller@gmail.com>
2016-02-11 16:37:23 -0200

    Adds Chorn Rate to admin#dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,57 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+    columns do
+      column do
+        panel "Últimas 10 compras realizadas HOJE" do
+          purchases = Purchase.where(status: PurchaseStatus::PAID,
+                                     created_at: 1.month.ago.beginning_of_day..1.month.ago.end_of_day)
+                              .last(10)
+          table do
+            th "Usuário"
+            th "Realizado em"
+            th "No valor de"
+            purchases.each do |purchase|
+              tr do
+                td link_to(purchase.user.name, admin_purchase_path(purchase))
+                td l purchase.created_at
+                td number_to_currency purchase.total
+              end
+            end
+          end
+
+          table do
+            th "Total"
+            tr do
+              td number_to_currency purchases.sum(&:total)
+            end
+          end
+        end
       end
+
+      column do
+        panel "Churn rate do mês de #{l 1.month.ago, format: '%B'}" do
+          month             = 1.month.ago.beginning_of_month..1.month.ago.end_of_month
+          new_users         = User.joins(:purchases).where(purchases: {created_at: month}).group('users.id').having('count(purchases.id) = 1').length
+          recurrent_users   = User.joins(:purchases).where(purchases: {created_at: month}).group('users.id').having('count(purchases.id) > 1').length
+          total_buyer_users = (new_users+recurrent_users)
+
+          table do
+            th "Total de usuários"
+            th "Usuários com 1 compra"
+            th "Usuários com 1+ compras"
+            th "Chorn Rate este mês"
+            tr do
+              td User.count
+              td new_users
+              td recurrent_users
+              td number_to_percentage(recurrent_users*100/total_buyer_users, precision: 2)
+            end
+          end
+        end
+      end
+
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
