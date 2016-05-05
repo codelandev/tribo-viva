bb5bc5abcacc696015b6e8c585668b84fb9144dc
Bruno Pazzim <bpazzim@gmail.com>
2016-05-05 17:46:15 -0300

    Remove validation specs on offer

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -4,14 +4,11 @@ RSpec.describe Offer, type: :model do
   describe "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :image }
-    it { should validate_presence_of :value }
     it { should validate_presence_of :stock }
     it { should validate_presence_of :producer }
     it { should validate_presence_of :description }
     it { should validate_presence_of :bank_account }
     it { should validate_presence_of :offer_ends_at }
-    it { should validate_presence_of :operational_tax }
-    it { should validate_presence_of :coordinator_tax }
     it { should validate_presence_of :collect_ends_at }
     it { should validate_presence_of :offer_starts_at }
     it { should validate_presence_of :collect_starts_at }
